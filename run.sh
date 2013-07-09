b4fa0741169d1a00a7f4a47409b6598a708604ef
Kanatoko <kinyuka@gmail.com>
2013-07-09 14:28:13 +0900

    conf files

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp lib/dozensHA.jar:lib/jumperz_util.jar:lib/jsonic-0.9.6.jar net.jumperz.app.MDozensHA.MDozensHA $1
+java -cp lib/dozensHA.jar:lib/jumperz_util.jar:lib/jsonic-0.9.6.jar net.jumperz.app.MDozensHA.MDozensHA http-example.conf --debug
